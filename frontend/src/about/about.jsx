1fac1743b4040cfc4eedd2705998320aa49889b0
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-07-10 20:45:54 -0300

    navegação

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,16 @@
 
 
-import React from "react"
-
+import React, {Component} from "react"
+import PageHeader from '../template/pageHeader'
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>What is Lorem Ipsum?
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
+        <h2>Imprensa</h2>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500</p>
     </div>
 )
