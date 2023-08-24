ff233d3edf86fa86191ddb4c7e8311356eab4027
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2023-08-24 17:25:18 -0300

    evento onchange

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,6 +1,6 @@
 
 
-import React, {Component} from "react"
+import React from "react"
 import PageHeader from '../template/pageHeader'
 export default props => (
     <div>
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,12 +1,37 @@
 
 import React, {Component} from 'react'
 import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleAdd() {
+        
+        console.log(this.state.description)
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}>
+                    </TodoForm>
+                <TodoList></TodoList>
             </div>
         )
     }
